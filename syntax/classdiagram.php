ee203782fff95df4fef79e76c16d53d3fda14fd8
Pavel Dvořák <pavel@pavel-dvorak.cz>
2017-07-02 22:30:29 +0200

    Structure plugin repair

--- syntax/classdiagram.php
+++ syntax/classdiagram.php
@@ -9,7 +9,7 @@
 if (!defined('DOKU_INC')) define('DOKU_INC', realpath(dirname(__FILE__) . '/../../') . '/');
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
 
-class syntax_plugin_yuml_classdiagram extends syntax_plugin_yuml
+class syntax_plugin_yuml_classdiagram extends syntax_plugin_yuml_usecase
 {
     protected $special_pattern = '<classdiagram.*?>.*?</classdiagram>';
     protected $handle_pattern = '/<classdiagram(.*?)>(.*)<\/classdiagram>/is';
